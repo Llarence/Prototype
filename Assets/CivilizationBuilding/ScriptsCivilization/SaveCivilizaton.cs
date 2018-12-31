ddcbbef4e3b330ea5359509471182e7a3b45417b
Llarence <36804301+Llarence@users.noreply.github.com>
2018-12-30 18:28:55 -0800

    work on saving

---
+++ Assets/CivilizationBuilding/ScriptsCivilization/SaveCivilizaton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[System.Serializable]
+public class gameState {
+	public float x;
+	public float y;
+	public float z;
+}
+
+
+public class SaveCivilizaton : MonoBehaviour {
+
+	string Path;
+	GameObject[] GameObjects;
+	public float[] Position;
+	FileStream Stream;
+	gameState GameState = new gameState();
+
+	public void Save (string SaveName) {
+		Path = Application.persistentDataPath + "/Player.Save";
+		GameObjects = FindObjectsOfType<GameObject>();
+		foreach (GameObject CurrentObject in GameObjects){
+			GameState.x = CurrentObject.transform.position.x;
+			GameState.y = CurrentObject.transform.position.y;
+			GameState.z = CurrentObject.transform.position.z;
+			File.WriteAllText (Path, JsonUtility.ToJson (GameState));
+		}
+	}
+}
