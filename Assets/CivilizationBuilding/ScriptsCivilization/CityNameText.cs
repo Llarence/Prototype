054c29ae83d38d40bb9f9e16a398671ad7a84459
Llarence <36804301+Llarence@users.noreply.github.com>
2019-01-12 13:24:03 -0800

    city

---
+++ Assets/CivilizationBuilding/ScriptsCivilization/CityNameText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityNameText : MonoBehaviour {
+
+	public string[] Names;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<TextMesh> ().text = Names[Random.Range(0, Names.Length + 1)];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
