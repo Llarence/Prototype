a3fe37ba86fd59e4ab46f360ef7e132571cfdd7c
Gary18 <tomtyler2005@gmail.com>
2019-01-12 15:42:39 -0800

    Update ManagerCivilization.cs

--- Assets/CivilizationBuilding/ScriptsCivilization/ManagerCivilization.cs
+++ Assets/CivilizationBuilding/ScriptsCivilization/ManagerCivilization.cs
@@ -90,19 +90,4 @@ public class ManagerCivilization : MonoBehaviour {
 
 		}
 	}
-
-	public void SetEditorBuildSettingsScenes()
-	{
-		// Find valid Scene paths and make a list of EditorBuildSettingsScene
-		List<EditorBuildSettingsScene> editorBuildSettingsScenes = new List<EditorBuildSettingsScene>();
-		foreach (var sceneAsset in m_SceneAssets)
-		{
-			string scenePath = AssetDatabase.GetAssetPath(sceneAsset);
-			if (!string.IsNullOrEmpty(scenePath))
-				editorBuildSettingsScenes.Add(new EditorBuildSettingsScene(scenePath, true));
-		}
-
-		// Set the Build Settings window Scene list
-		EditorBuildSettings.scenes = editorBuildSettingsScenes.ToArray();
-	}
 }
