350338fda78da681ac462944dcd7d6853d21c8a5
Llarence <36804301+Llarence@users.noreply.github.com>
2019-01-12 14:20:40 -0800

    Update CityNameText.cs

--- Assets/CivilizationBuilding/ScriptsCivilization/CityNameText.cs
+++ Assets/CivilizationBuilding/ScriptsCivilization/CityNameText.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class CityNameText : MonoBehaviour {
 
 	public string[] Names;
+	public string OverideName;
 
 	// Use this for initialization
 	void Start () {
 		GetComponent<TextMesh> ().text = Names[Random.Range(0, Names.Length)];
+		if(OverideName != ""){
+			GetComponent<TextMesh> ().text = OverideName;
+		}
 	}
 	
 	// Update is called once per frame
