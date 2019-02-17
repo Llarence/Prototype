8b4c29f85c6f081eb418e56c9c821fb3f207cdce
Llarence <36804301+Llarence@users.noreply.github.com>
2019-02-17 11:48:39 -0800

    Update CityCivilization.cs

--- Assets/CivilizationBuilding/ScriptsCivilization/CityCivilization.cs
+++ Assets/CivilizationBuilding/ScriptsCivilization/CityCivilization.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CityCivilization : MonoBehaviour {
 
@@ -10,6 +11,14 @@ public class CityCivilization : MonoBehaviour {
 	float clickTime;
 	public GameObject border;
 	public int turnIAmOn;
+	public int Farms;
+	public int Houses;
+	public int GoldMines;
+	public int Storages;
+	public int People;
+	public int Food;
+	public int Population;
+	public int Gold;
 
 	// Use this for initialization
 	void Start () {
@@ -42,11 +51,35 @@ public class CityCivilization : MonoBehaviour {
 		}
 		if(turnIAmOn < manager.GetComponent<ManagerCivilization>().turn){
 			turnIAmOn++;
+			Calculate ();
 			AddBorder ();
 		}	
 	}
 
 	void AddBorder (){
 		//Instantiate (border, transform.position + new Vector3(10, -2.45f, -20), Quaternion.identity);
+	}
+
+	void Calculate (){
+		Food += Mathf.Clamp(Mathf.Clamp (Population, 0, Houses * 1000000), 0, Farms * 2) * 2;
+		Food -= Population;
+		if(Food < 0){
+			Population += Food;
+			Food = 0;
+		}
+		Population += Mathf.FloorToInt(Mathf.Clamp (Food, 0, Population/4));
+		Population = Mathf.Clamp (Population, 4, Houses * 3 + 4);
+		Gold += Mathf.FloorToInt(Mathf.Clamp (Mathf.Clamp(Population, 0, Houses * 1000000) - Mathf.Clamp(Population, 0, Farms.Length * 2), 0, GoldMines.Length * 2));
+		Food = Mathf.FloorToInt(Mathf.Clamp (Food, 0, Storages * 12));
+		Gold = Mathf.FloorToInt(Mathf.Clamp (Gold, 0, (Storages * 12) + 10));
+		GameObject.Find ("Resources").GetComponent<Text> ().text = "Gold:" + Gold;
+		while(People < Population){
+			Instantiate (Person, new Vector3(0, 0.5f, 0), Quaternion.identity);
+			People = GameObject.FindGameObjectsWithTag ("Person");
+		}
+		if (People > Population){
+			Destroy (GameObject.FindGameObjectWithTag ("Person"));
+			People = GameObject.FindGameObjectsWithTag ("Person");
+		}
 	}
 }
