bcdeb8649afbe48fbfd018b950f1f8cdc805d6fd
Gary18 <tomtyler2005@gmail.com>
2019-04-12 17:54:00 -0700

    VidTutorialsUpdate 1.0

---
+++ Assets/Tutorial Vids/TutCivScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TutCivScript : MonoBehaviour {
+
+    public bool playTutorial;
+
+    // Use this for initialization
+    void Start() {
+        GameObject.Find("Quad").GetComponent<VideoPlayer>().Pause();
+        GameObject.Find("Quad").GetComponent<Transform>().transform.Rotate (0, 90, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       if (playTutorial == true)
+        {
+            GameObject.Find("Quad").GetComponent<Transform>().transform.Rotate(0, -90, 0);
+            GameObject.Find("Quad").GetComponent<VideoPlayer>().Play();
+        }
+       if (playTutorial == false)
+        {
+            GameObject.Find("Quad").GetComponent<Transform>().transform.Rotate(0, 90, 0);
+            GameObject.Find("Quad").GetComponent<VideoPlayer>().Stop();
+        }
+	}
+    public void SpawnTutorialVid()
+    {
+        playTutorial = true;
+    }
+    public void OffTutorialVids()
+    {
+        playTutorial = false;
+    }
+}
